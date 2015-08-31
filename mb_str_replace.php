dc118d1526ab117f48b0c297823866149429df56
S.Yoshida <fromage.blanc.music@gmail.com>
2015-08-31 18:10:52 +0900

    Update mb_str_replace.php

--- mb_str_replace.php
+++ mb_str_replace.php
@@ -2,9 +2,13 @@
 /* 日本語対応str_replace */
 function mb_str_replace($search, $replace, $haystack, $encoding="UTF-8")
 {
+                // 検索先は配列か？
                 $notArray = !is_array($haystack) ? TRUE : FALSE;
+                // コンバート
                 $haystack = $notArray ? array($haystack) : $haystack;
+                // 検索文字列の文字数取得
                 $search_len = mb_strlen($search, $encoding);
+                // 置換文字列の文字数取得
                 $replace_len = mb_strlen($replace, $encoding);
                 foreach ($haystack as $i => $hay){
                                 $offset = mb_strpos($hay, $search);
