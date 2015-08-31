e72c6fc53f0aca7b0442d4f41a2b48248a6f4949
S.Yoshida <fromage.blanc.music@gmail.com>
2015-08-31 18:20:09 +0900

    Update mb_str_replace.php

--- mb_str_replace.php
+++ mb_str_replace.php
@@ -10,9 +10,13 @@ function mb_str_replace($search, $replace, $haystack, $encoding="UTF-8")
                 $search_len = mb_strlen($search, $encoding);
                 // 置換文字列の文字数取得
                 $replace_len = mb_strlen($replace, $encoding);
+                
                 foreach ($haystack as $i => $hay){
+                                // マッチング
                                 $offset = mb_strpos($hay, $search);
+                                // 一致した場合
                                 while ($offset !== FALSE){
+                                                // 差替え処理
                                                 $hay = mb_substr($hay, 0, $offset).$replace.mb_substr($hay, $offset + $search_len);
                                                 $offset = mb_strpos($hay, $search, $offset + $replace_len);
                                 }
@@ -21,7 +25,9 @@ function mb_str_replace($search, $replace, $haystack, $encoding="UTF-8")
                 return $notArray ? $haystack[0] : $haystack;
 }
 
+/* test */
 $search = "あいうえお";
 $replace = "かきくけこ";
 
 var_dump(mb_str_replace($search,$replace,"あいうえお","UTF-8")); // かきくけこ
+
